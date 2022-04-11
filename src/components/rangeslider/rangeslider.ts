2da853a6b19f2d38e6494c40839df4539944105c
Ekaterina <1988snea@gmail.com>
2022-04-11 23:42:19 +0500

    component Rangeslider fixed in line with ESLint rules

--- src/components/rangeslider/rangeslider.ts
+++ src/components/rangeslider/rangeslider.ts
@@ -65,10 +65,10 @@ class RangeSlider {
   constructor(selector: string, elem: Element) {
     this.selector = selector;
     this.wrapper = elem as HTMLElement;
-    this.panel = this.wrapper.querySelector('.js-panel') as HTMLElement;
-    this.slider = this.wrapper.querySelector('.js-rs-metalamp') as HTMLElement;
-    this.panelWrapper = this.wrapper.querySelector(`${this.selector}__panel`) as HTMLElement;
-    this.sliderWrapper = this.wrapper.querySelector(`${this.selector}__rs`) as HTMLElement;
+    this.panel = RangeSlider.getElem(this.wrapper, '.js-panel') as HTMLElement;
+    this.slider = RangeSlider.getElem(this.wrapper, '.js-rs-metalamp') as HTMLElement;
+    this.panelWrapper = RangeSlider.getElem(this.wrapper, `${this.selector}__panel`) as HTMLElement;
+    this.sliderWrapper = RangeSlider.getElem(this.wrapper, `${this.selector}__rs`) as HTMLElement;
     this.renderPanel();
     this.renderSlider(this.slider);
     this.updateSlider();
@@ -77,11 +77,15 @@ class RangeSlider {
     this.subscribeSlider();
   }
 
-  private valid(
+  static getElem(obj: HTMLElement, selector: string) {
+    return obj.querySelector(selector);
+  }
+
+  static valid(
     item: HTMLInputElement,
     val: number | string | boolean,
   ) {
-    if (item.value != val) { item.value = val as string; }
+    if (item.value !== val) { item.value = val as string; }
   }
 
   private switchVertical() {
@@ -91,82 +95,56 @@ class RangeSlider {
   }
 
   private displayData(data: IConf) {
-    if (this.min
-      && this.max
-      && this.from
-      && this.to
-      && this.interval
-      && this.step
-      && this.shiftOnKeyDown
-      && this.shiftOnKeyHold
-      && this.vertical
-      && this.range
-      && this.scale
-      && this.bar
-      && this.tip
-      && this.sticky
-      && this.subscribe
-    ) {
-      const D = data;
-      this.min.value = String(D.min);
-      this.max.value = String(D.max);
-      this.from.value = String(D.from);
-      this.to.value = String(D.to);
-      this.interval.value = String(D.interval);
-      this.step.value = String(D.step);
-      this.shiftOnKeyDown.value = String(D.shiftOnKeyDown);
-      this.shiftOnKeyHold.value = String(D.shiftOnKeyHold);
-      this.vertical.checked = !!D.vertical;
-      this.range.checked = !!D.range;
-      this.scale.checked = !!D.scale;
-      this.bar.checked = !!D.bar;
-      this.tip.checked = !!D.tip;
-      this.sticky.checked = !!D.sticky;
-      this.subscribe.checked = true;
+    const D = data;
+    if (this.min) this.min.value = String(D.min);
+    if (this.max) this.max.value = String(D.max);
+    if (this.from) this.from.value = String(D.from);
+    if (this.to) this.to.value = String(D.to);
+    if (this.interval) this.interval.value = String(D.interval);
+    if (this.step) this.step.value = String(D.step);
+    if (this.shiftOnKeyDown) this.shiftOnKeyDown.value = String(D.shiftOnKeyDown);
+    if (this.shiftOnKeyHold) this.shiftOnKeyHold.value = String(D.shiftOnKeyHold);
+    if (this.vertical) this.vertical.checked = !!D.vertical;
+    if (this.range) this.range.checked = !!D.range;
+    if (this.scale) this.scale.checked = !!D.scale;
+    if (this.bar) this.bar.checked = !!D.bar;
+    if (this.tip) this.tip.checked = !!D.tip;
+    if (this.sticky) this.sticky.checked = !!D.sticky;
+    if (this.subscribe) this.subscribe.checked = true;
 
-      if (data.scaleBase == 'interval' && this.scaleBaseIntervals) {
-        this.scaleBaseIntervals.checked = true;
-        this.step.disabled = true;
-      }
-
-      if (data.scaleBase == 'step' && this.scaleBaseSteps) {
-        this.scaleBaseSteps.checked = true;
-        this.interval.disabled = true;
-      }
-      this.to.disabled = !D.range;
+    if (data.scaleBase === 'interval' && this.scaleBaseIntervals) {
+      this.scaleBaseIntervals.checked = true;
+      if (this.step) this.step.disabled = true;
     }
+
+    if (data.scaleBase === 'step' && this.scaleBaseSteps) {
+      this.scaleBaseSteps.checked = true;
+      if (this.interval) this.interval.disabled = true;
+    }
+    if (this.to) this.to.disabled = !D.range;
   }
 
   private updateData = (data: IConf) => {
     if (data.vertical
-      != this.wrapper.classList.contains('rangeslider_vert')) {
+      !== this.wrapper.classList.contains('rangeslider_vert')) {
       this.switchVertical();
     }
     const D = data;
-    if (this.from
-      && this.to
-      && this.min
-      && this.max
-      && this.shiftOnKeyDown
-      && this.shiftOnKeyHold
-      && this.interval
-      && this.step) {
-      this.valid(this.from, Number(D.from));
-      this.valid(this.to, Number(D.to));
-      this.valid(this.min, Number(D.min));
-      this.valid(this.max, Number(D.max));
-      this.valid(this.shiftOnKeyDown, Number(D.shiftOnKeyDown));
-      this.valid(this.shiftOnKeyHold, Number(D.shiftOnKeyHold));
-      this.valid(this.interval, Number(D.interval));
-      this.valid(this.step, Number(D.step));
-    }
+    if (this.from) RangeSlider.valid(this.from, Number(D.from));
+    if (this.to) RangeSlider.valid(this.to, Number(D.to));
+    if (this.min) RangeSlider.valid(this.min, Number(D.min));
+    if (this.max) RangeSlider.valid(this.max, Number(D.max));
+    if (this.shiftOnKeyDown) RangeSlider.valid(this.shiftOnKeyDown, Number(D.shiftOnKeyDown));
+    if (this.shiftOnKeyHold) RangeSlider.valid(this.shiftOnKeyHold, Number(D.shiftOnKeyHold));
+    if (this.interval) RangeSlider.valid(this.interval, Number(D.interval));
+    if (this.step) RangeSlider.valid(this.step, Number(D.step));
   };
 
   private changeData(data: IConf) {
     const D = data;
     if (this.from && this.to) {
-      this.valid(this.from, Number(D.from));
-      this.valid(this.to, Number(D.to));
+      RangeSlider.valid(this.from, Number(D.from));
+      RangeSlider.valid(this.to, Number(D.to));
     }
   }
 
@@ -217,16 +195,15 @@ class RangeSlider {
 
     this.scaleBaseSteps = getElem('radio-step');
     this.scaleBaseIntervals = getElem('radio-interval');
-
-    for (const elem of this.inputsAll) {
-      elem.addEventListener('change', () => {
-        console.log(elem.value);
-
-        if (!elem.value) {
-          elem.value = '0';
-        }
-      });
-    }
+    const changeHandler = (e: Event) => {
+      const input = e.currentTarget as HTMLInputElement;
+      if (!input.value) {
+        input.value = '0';
+      }
+    };
+    this.inputsAll.forEach((elem) => {
+      elem.addEventListener('change', changeHandler);
+    });
   }
 
   private renderSlider(slider: HTMLElement) {
@@ -253,115 +230,115 @@ class RangeSlider {
       'tip',
       'scaleBase',
     ];
-    this.panel.addEventListener('change', (e) => {
+    const handleChange = (e: Event) => {
       if (!this.isDestroyed) {
         const target = e.target as HTMLInputElement;
-        for (const elem of arr) {
+        arr.forEach((elem) => {
           if (target.closest(`.panel__${elem}`)) {
             let value;
-            if (target.type == 'checkbox') {
+            if (target.type === 'checkbox') {
               value = target.checked;
-            } else if (target.type == 'radio') {
+            } else if (target.type === 'radio') {
               value = target.value;
-            } else if (!isNaN(+target.value)) {
+            } else if (!Number.isNaN(+target.value)) {
               value = parseFloat(target.value);
             } else value = 0;
             this.rangeSlider.update({ [elem]: value });
-            if (elem == 'scaleBase') {
-              if (value == 'interval' && this.interval && this.step) {
-                this.interval.disabled = false;
-                this.step.disabled = true;
+            if (elem === 'scaleBase') {
+              if (value === 'interval') {
+                if (this.interval) this.interval.disabled = false;
+                if (this.step) this.step.disabled = true;
               }
-              if (value == 'step' && this.interval && this.step) {
-                this.interval.disabled = true;
-                this.step.disabled = false;
+              if (value === 'step') {
+                if (this.interval) this.interval.disabled = true;
+                if (this.step) this.step.disabled = false;
               }
             }
-            if (elem == 'range' && this.to) {
+            if (elem === 'range' && this.to) {
               this.to.disabled = !target.checked;
             }
-            break;
+            //   break;
           }
-        }
+        });
       }
-    });
+    };
+    this.panel.addEventListener('change', handleChange);
   }
 
   // API disable
   private disableSlider() {
-    if (this.disable) {
-      this.disable.addEventListener('click', () => {
-        if (!this.isDestroyed && this.disable) {
-          if (this.disable.checked) {
-            this.rangeSlider.disable();
-            for (const elem of this.inputsAll) {
-              elem.disabled = true;
-            }
-            this.isDisabled = true;
-          } else {
-            this.rangeSlider.enable();
-            for (const elem of this.inputsAll) {
-              elem.disabled = false;
-            }
-            const data = this.rangeSlider.getData();
-            this.displayData(data);
-            this.isDisabled = false;
+    const handleClick = () => {
+      if (!this.isDestroyed && this.disable) {
+        if (this.disable.checked) {
+          this.rangeSlider.disable();
+          for (const elem of this.inputsAll) {
+            elem.disabled = true;
           }
+          this.isDisabled = true;
+        } else {
+          this.rangeSlider.enable();
+          for (const elem of this.inputsAll) {
+            elem.disabled = false;
+          }
+          const data = this.rangeSlider.getData();
+          this.displayData(data);
+          this.isDisabled = false;
         }
-      });
+      }
+    };
+    if (this.disable) {
+      this.disable.addEventListener('click', handleClick);
     }
   }
 
   private subscribeSlider() {
-    if (this.subscribe) {
-      this.subscribe.addEventListener('click', () => {
-        if (!this.isDestroyed && this.subscribe) {
-          if (this.subscribe.checked) {
-            this.rangeSlider.update({
-              onChange: (data: IConf) => {
-                this.changeData(data);
-              },
-            });
-          } else {
-            this.rangeSlider.update({
-              onChange: null,
-            });
-          }
+    const handleClick = () => {
+      if (!this.isDestroyed && this.subscribe) {
+        if (this.subscribe.checked) {
+          this.rangeSlider.update({
+            onChange: (data: IConf) => {
+              this.changeData(data);
+            },
+          });
+        } else {
+          this.rangeSlider.update({
+            onChange: null,
+          });
         }
-      });
+      }
+    };
+    if (this.subscribe) {
+      this.subscribe.addEventListener('click', handleClick);
     }
   }
 
   // API destroy
   private destroySlider(slider: HTMLElement) {
-    if (this.destroy) {
-      this.destroy.addEventListener('click', () => {
-        if (this.destroy && this.disable) {
-          if (this.destroy.checked) {
-            this.rangeSlider.destroy();
-            for (const elem of this.inputsAll) {
-              elem.disabled = true;
-            }
-            this.disable.checked = true;
-            this.disable.disabled = true;
-            this.isDestroyed = true;
-            this.destroy.disabled = true;
-            // отвязать объект слайдера от DOM-элемента
-            $.data(slider, 'SliderMetaLamp', null);
+    const handleClick = () => {
+      if (this.destroy && this.disable) {
+        if (this.destroy.checked) {
+          this.rangeSlider.destroy();
+          for (const elem of this.inputsAll) {
+            elem.disabled = true;
           }
+          this.disable.checked = true;
+          this.disable.disabled = true;
+          this.isDestroyed = true;
+          this.destroy.disabled = true;
+          // отвязать объект слайдера от DOM-элемента
+          $.data(slider, 'SliderMetaLamp', null);
         }
-      });
+      }
+    };
+    if (this.destroy) {
+      this.destroy.addEventListener('click', handleClick);
     }
   }
 }
 
 function renderRangeSliders(selector: string) {
   const rangeSliders = document.querySelectorAll(selector);
-  const mas: RangeSlider[] = [];
-  for (const rangeSlider of rangeSliders) {
-    mas.push(new RangeSlider(selector, rangeSlider));
-  }
-  return mas;
+  rangeSliders.forEach((rangeSlider) => new RangeSlider(selector, rangeSlider));
 }
 
 renderRangeSliders('.js-rangeslider');
