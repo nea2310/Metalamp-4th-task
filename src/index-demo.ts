c7b043192b1fbfd0d190ea526608c5de66bc46e7
Ekaterina <1988snea@gmail.com>
2022-12-06 18:58:32 +0500

    Moved css property box-sizing: border-box to global level #19

--- src/index-demo.ts
+++ src/index-demo.ts
@@ -5,5 +5,6 @@ function requireAll(requireContext: __WebpackModuleApi.RequireContext) {
   return requireContext.keys().map(requireContext);
 }
 
+requireAll(require.context('./assets/styles/', true, /.*\.(scss)$/));
 requireAll(require.context('./components/', true, /.*\.(ts|scss)$/));
 requireAll(require.context('./pages/', true, /.*\.(ts|scss)$/));
