792d0b18222c20e4e741e22e2755135f2dd2ff42
Ekaterina <1988snea@gmail.com>
2022-12-12 18:19:38 +0500

    Added separate BEM element radiobuttons__category #18

--- src/components/radiobuttons/Radiobuttons.ts
+++ src/components/radiobuttons/Radiobuttons.ts
@@ -1,16 +1,19 @@
 class Radiobuttons {
   private wrapper: HTMLElement;
 
+  private elementName: string;
+
   private radioMarks: NodeListOf<HTMLInputElement> | null = null;
 
-  constructor(element: Element) {
+  constructor(element: Element, elementName = 'radiobuttons') {
     this.wrapper = element as HTMLElement;
+    this.elementName = elementName;
     this.render();
     this.addEventListeners();
   }
 
   private render() {
-    this.radioMarks = this.wrapper.querySelectorAll('.js-category-radiomark') as NodeListOf<HTMLInputElement>;
+    this.radioMarks = this.wrapper.querySelectorAll(`.js-${this.elementName}__category-radiomark`) as NodeListOf<HTMLInputElement>;
   }
 
   private addEventListeners() {
@@ -21,9 +24,9 @@ class Radiobuttons {
 
   private handleCheckMarkKeydown = (event: KeyboardEvent) => {
     const target = event.target as HTMLInputElement;
-    const parent = target.closest('.category');
+    const parent = target.closest(`.js-${this.elementName}__category`);
     if (!parent || event.code === 'Tab') return false;
-    const radiobuttonHidden = parent.querySelector('.category-checkbox') as HTMLInputElement;
+    const radiobuttonHidden = parent.querySelector(`.js-${this.elementName}__category-checkbox`) as HTMLInputElement;
     event.preventDefault();
     if (event.code === 'Space' && target.previousElementSibling) {
       radiobuttonHidden.click();
