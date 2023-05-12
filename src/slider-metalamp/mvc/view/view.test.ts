530b54708141ff0c90d5965d8a89e5d43c709b49
Ekaterina <1988snea@gmail.com>
2023-05-12 16:43:09 +0500

    Enhanced test coverage for view #25

--- src/slider-metalamp/mvc/view/view.test.ts
+++ src/slider-metalamp/mvc/view/view.test.ts
@@ -1,28 +1,60 @@
 import { createInstance } from '../test-utils';
+import { IPluginConfigurationFull } from '../interface';
 
-describe('', () => {
-  test('', () => {
+const configuration: Partial<IPluginConfigurationFull> = {
+  min: -100,
+  max: 100,
+  from: -50,
+  to: 50,
+  vertical: true,
+  range: false,
+  bar: false,
+  tip: false,
+  scale: false,
+  scaleBase: 'interval',
+  step: 1,
+  interval: 20,
+  sticky: true,
+  shiftOnKeyDown: 2,
+  shiftOnKeyHold: 2,
+  round: 1000000000000000,
+};
+
+describe('Should initialize slider instance with correct parameters', () => {
+  test('Should initialize slider instance with parameters taken from configuration object', () => {
     const {
       testController,
+      testModel,
     } = createInstance();
 
-    testController.update({
-      min: -100,
-      max: 100,
-      from: -50,
-      to: 50,
-      vertical: true,
-      range: false,
-      bar: false,
-      tip: false,
-      scale: false,
-      scaleBase: 'interval',
-      step: 1,
-      interval: 20,
-      sticky: true,
-      shiftOnKeyDown: 2,
-      shiftOnKeyHold: 2,
-      round: 1,
-    });
+    const updateModel = jest.spyOn(testModel, 'update');
+
+    testController.update(configuration);
+
+    expect(updateModel).toHaveBeenLastCalledWith(
+      expect.objectContaining({ ...configuration, step: 10, round: 0 }),
+    );
+  });
+
+  test('Should initialize slider instance with parameters taken from data attributes', () => {
+    const {
+      testModel,
+      testController,
+    } = createInstance({}, [
+      { name: 'min', value: '-200' },
+      { name: 'max', value: '200' },
+    ]);
+
+    const updateModel = jest.spyOn(testModel, 'update');
+
+    testController.update({ step: 20 });
+
+    expect(updateModel).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        min: -200,
+        max: 200,
+        step: 20,
+      }),
+    );
   });
 });
